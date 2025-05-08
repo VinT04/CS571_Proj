e18dfcd983125f4f331fe116ab01c7b990736646
Jake Reid <jbreid@umass.edu>
2025-05-08 14:12:37 -0400

    text visiblity update

--- src/stateGraphs.js
+++ src/stateGraphs.js
@@ -101,7 +101,8 @@ export function createStateGraphs (stateName) {
       g.append('g')
         .call(yAxis)
         .selectAll('text')              
-          .attr('font-size', '0.9rem');  
+          .attr('font-size', '0.9rem')
+          .attr('fill', 'white');
     
       const xAxis = d3.axisBottom(x)
         .tickFormat(d => d + '%');
@@ -109,7 +110,8 @@ export function createStateGraphs (stateName) {
         .attr('transform', `translate(0,${innerH})`)
         .call(xAxis)
         .selectAll('text')            
-          .attr('font-size', '0.9rem');  
+          .attr('font-size', '0.9rem')
+          .attr('fill', 'white');
 
       g.selectAll('rect')
       .data(dataset)
@@ -143,6 +145,7 @@ export function createStateGraphs (stateName) {
         .attr('y', margin.top - 20)
         .attr('font-size', '1.25rem')
         .attr('font-weight', 700)
+        .attr('fill', 'white')
         .text(title);
     }
 
@@ -179,14 +182,16 @@ export function createStateGraphs (stateName) {
     .attr('transform', `translate(0,${innerH})`)
     .call(xAxis)
     .selectAll('text')                  
-    .attr('font-size', '0.9rem');     
+    .attr('font-size', '0.9rem')
+    .attr('fill', 'white');
 
   const yAxis = d3.axisLeft(y)
     .tickFormat(d => d + '%');
   g.append('g')
     .call(yAxis)
     .selectAll('text')                 
-      .attr('font-size', '0.9rem');
+      .attr('font-size', '0.9rem')
+      .attr('fill', 'white');
       const line = d3.line()
         .defined(d => d.value !== null)
         .x(d => x(d.date))
@@ -226,6 +231,7 @@ export function createStateGraphs (stateName) {
         .attr('y', margin.top - 20)
         .attr('font-size', '1.25rem')
         .attr('font-weight', 700)
+        .attr('fill', 'white')
         .text(title);
 
       // legend 
@@ -257,6 +263,7 @@ export function createStateGraphs (stateName) {
         .attr('x', 20)
         .attr('y', 11)
         .attr('font-size', '0.85rem')
+        .attr('fill', 'white')
         .text(d => d.name);
     }
     const selectedDemographic = document.getElementById('Dem-option');
